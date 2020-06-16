b821c535e969d2fe314df772a4c36a0303df0081
ashwin <ashwinram472@gmail.com>
2020-06-16 00:43:30 -0400

    added connect = False

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "b'\xe0{\xed\xac\xab\xf6Vy\xf4\xd7r\xcc\\\xc7|q'"
 
     MONGODB_SETTINGS = { 'db' : 'DiamondApp',
-                        'host' : mongodb_uri}
+                        'host' : mongodb_uri,
+                        'connect' : False}
